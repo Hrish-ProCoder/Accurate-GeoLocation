04229cda5f01e84304447c18d92fcd5b324bf559
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-10-15 07:52:39 +0530

    Added with wait time

--- Accurate Geo Location Finder.py
+++ Accurate Geo Location Finder.py
@@ -10,3 +10,9 @@ def getLocation():
     driver = webdriver.Chrome(executable_path='C:/Users/hrishabh/PycharmProjects/Acurate Geolocation/chromedriver.exe', options=options)
     # Store chromedriver.exe in ur python file location itself
     # Edit the path accordingly
+    
+    #Wait Time to load the page
+    timeout = 20
+    driver.get("https://mycurrentlocation.net/")
+    wait = WebDriverWait(driver, timeout)
+    time.sleep(3)
